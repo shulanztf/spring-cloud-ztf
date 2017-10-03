5cbe080ab68280901ca1f5921b22f09290bc9f4f
1426763507@qq.com <1426763507@qq.com>
2017-10-03 09:38:38 +0800

    spring-cloud 初始提交

---
+++ spring-cloud-config-server/src/main/java/com/cloud/backend/hhcf/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.cloud.backend.hhcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @ClassName: ConfigServerApplication
+ * @Description:
+ * @see http://blog.csdn.net/forezp/article/details/70148833
+ * @author: zhaotf
+ * @date: 2017年10月3日 上午9:18:11
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
