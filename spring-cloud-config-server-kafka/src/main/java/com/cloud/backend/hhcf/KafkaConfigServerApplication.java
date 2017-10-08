10a6c351561e5a506a48dc9f55d0ca94b4c17ddb
1426763507@qq.com <1426763507@qq.com>
2017-10-08 20:13:31 +0800

    spring-cloud-config+netflix-bus+kafka

---
+++ spring-cloud-config-server-kafka/src/main/java/com/cloud/backend/hhcf/KafkaConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.cloud.backend.hhcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @ClassName: KafkaConfigServerApplication
+ * @Description:
+ * @see http://www.jianshu.com/p/730d86030a41
+ * @see http://www.cnblogs.com/zhangjianbin/p/6347247.html
+ * @author: zhaotf
+ * @date: 2017年10月8日 下午7:03:42
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class KafkaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConfigServerApplication.class, args);
+	}
+
+}
