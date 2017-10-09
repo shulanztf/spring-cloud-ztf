d9416924f0181429d5efb19aa6a1de4a3539b685
1426763507@qq.com <1426763507@qq.com>
2017-10-09 21:40:38 +0800

    spring-cloud-config+netflix-bus+kafka

--- spring-cloud-config-server-kafka/src/main/java/com/cloud/backend/hhcf/KafkaConfigServerApplication.java
+++ spring-cloud-config-server-kafka/src/main/java/com/cloud/backend/hhcf/KafkaConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.cloud.backend.hhcf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 
@@ -15,6 +16,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class KafkaConfigServerApplication {
 
 	public static void main(String[] args) {
